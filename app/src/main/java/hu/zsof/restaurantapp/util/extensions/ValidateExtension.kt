30940bf974ab510621ebf62f7bea1222421c7504
Kárpáti Zsófia Viktória <zsofi.viki@gmail.com>
2022-10-13 11:15:51 +0200

    change name

--- app/src/main/java/hu/zsof/restaurantapp/util/extensions/ValidateExtension.kt
+++ app/src/main/java/hu/zsof/restaurantapp/util/extensions/ValidateExtension.kt
@@ -3,7 +3,7 @@ package hu.zsof.restaurantapp.util.extensions
 import android.widget.EditText
 import hu.zsof.restaurantapp.R
 
-fun EditText.validateNonEmpty(): Boolean {
+fun EditText.validateNonEmptyField(): Boolean {
     if (text.isEmpty()) {
         error = context.getString(R.string.require_error)
         return false
